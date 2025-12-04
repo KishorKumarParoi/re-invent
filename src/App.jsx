0c43862abc8be40691c10d624c247136dbcbb8cb
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2025-12-04 16:48:10 +0600

    fix: solved vercel analytics bug

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,19 @@ import React from 'react'
 import Hero from './components/Hero'
 import About from './components/About'
 import Bio from './components/Bio'
-import { Analytics } from "@vercel/analytics/next"
+import { inject } from '@vercel/analytics'
+
+// Initialize Vercel Analytics for web
+inject()
 
 const App = () => {
   return (
     <main className='relative min-h-screen w-screen overflow-x-hidden'>
-      <Analytics />
       <Hero />
       <Bio />
       <About />
-      <section className='z-0 min-h-screen text-blue-500 text-7xl flex-center' >
-        Working Under Progess
+      <section className='z-0 min-h-screen text-blue-500 text-7xl flex-center'>
+        Working Under Progress
       </section>
     </main>
   )
